e153f8f205719aa061751abf89734ca7a883965d
Joaquin Blanco <blancojoaquin77@gmail.com>
2022-10-14 01:19:49 -0300

    update Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,39 +2,35 @@ import { Link, Stack, Typography, useMediaQuery } from "@mui/material";
 import { Facebook, Twitter, Instagram, Phone } from "@mui/icons-material";
 
 const Footer = () => {
-  const sm = useMediaQuery("(min-width: 600px)");
   return (
     <Stack
       spacing={2}
       height={"100%"}
       alignItems={"center"}
       justifyContent={"center"}
+      py={"2rem"}
     >
-      <Typography
-        variant={sm ? "h4" : "h5"}
-        className="subtitle bold"
-        textAlign={"center"}
-      >
+      <Typography variant={"h5"} className="subtitle bold" textAlign={"center"}>
         ¿Querés comunicarte con nosotros?
       </Typography>
       <Stack direction={"row"} spacing={5}>
         <Link href="https://es-la.facebook.com/amhclinicas/" color={"inherit"}>
-          <Facebook fontSize="large" color="inherit" />
+          <Facebook color="inherit" />
         </Link>
         <Link
           href="https://twitter.com/Prensaclinicas?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
           color={"inherit"}
         >
-          <Twitter fontSize="large" color="inherit" />
+          <Twitter color="inherit" />
         </Link>
         <Link
           href="https://www.instagram.com/hospitaldeclinicasok/?hl=es"
           color={"inherit"}
         >
-          <Instagram fontSize="large" color="inherit" />
+          <Instagram color="inherit" />
         </Link>
         <Link href="tel:011 5950-8000" color={"inherit"}>
-          <Phone fontSize="large" color="inherit" />
+          <Phone color="inherit" />
         </Link>
       </Stack>
     </Stack>
