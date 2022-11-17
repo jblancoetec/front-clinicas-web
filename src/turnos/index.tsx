cc8de3b01eaaffb285b6256c3bf598bb1d9e00c2
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2022-11-17 13:48:35 -0300

    Update index.tsx

--- src/turnos/index.tsx
+++ src/turnos/index.tsx
@@ -3,23 +3,21 @@ import Formulario from "./components/FormularioTurnos";
 import { Turno } from "./interfaces";
 import { getTurnosDisponible } from "./service";
 
-const index = () => {
+const Index = () => {
   const [turnosDisponibles, setTurnosDisponibles] = useState<string[]>([]);
   useEffect(() => {
     async function get() {
       try {
         const res = await getTurnosDisponible();
-        setTurnosDisponibles(res.turnos);   
+        setTurnosDisponibles(res.turnos);
       } catch (error) {
         console.log("error");
       }
-     
     }
     get();
-  },[]
-  );
+  }, []);
 
   return <Formulario turnos={turnosDisponibles} />;
 };
 
-export default index;
+export default Index;
