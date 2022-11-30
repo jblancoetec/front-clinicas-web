92e8b32dda8a937e2abd58e06f3fc18bf4b59100
Joaquin Blanco <jblanco@etec.uba.ar>
2022-11-30 10:49:44 -0300

    refactor: move Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,44 @@ import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
 import Layout from "@/src/layout/Layout";
 import { CssBaseline } from "@mui/material";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Fragment>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#7e57c2" />
+        <meta name="theme-color" content="#7e57c2" />
+        <title>Departamento de Hemoterapia - Hospital de Clinicas</title>
+        <meta
+          name="description"
+          content="Departamento de Hemoterapia - Hospital de Clinicas"
+        />
+      </Head>
       <CssBaseline />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </Fragment>
   );
 }
 
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { Fragment, ReactNode } from "react";
 import NavDesktop from "./NavDesktop";
 import NavMobile from "./NavMobile";
@@ -8,34 +7,6 @@ import { Box } from "@mui/material";
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <Fragment>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#7e57c2" />
-        <meta name="theme-color" content="#7e57c2" />
-        <title>Departamento de Hemoterapia - Hospital de Clinicas</title>
-        <meta
-          name="description"
-          content="Departamento de Hemoterapia - Hospital de Clinicas"
-        />
-      </Head>
-
       <header>
         <Box height="8vh" width={"100%"} className="d-desktop">
           <NavDesktop />
